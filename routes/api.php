762f40e34992f69ccff295a914cec51f547e4ca4
arbab <hessam.arbab@gmail.com>
2023-08-23 19:15:37 +0330

    added some simple apis

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\OrderController;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('users', function () {
+    return User::all();
 });
+
+Route::get('products', function () {
+    return Product::all();
+});
+
